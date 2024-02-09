2bb23a06d2816c935897fe151a41854a365b8e4d
Simon Gussenhoven <simongussenhoven@gmail.com>
2024-02-10 00:47:07 +0100

    Update yml

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 import app from './app';
-
-mongoose.connect(process.env.DB_PATH).then(() => console.log('db connection succesfull'));
+const path = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@smikkelweb.n7gcekz.mongodb.net/smikkelweb?retryWrites=true&w=majority`
+mongoose.connect(path).then(() => console.log('db connection succesfull'));
 
 const port = process.env.BACKEND_PORT;
 app.listen(port, () => {
