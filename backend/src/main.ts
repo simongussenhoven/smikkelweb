054a24ae4daa69411f9f41ed3e6145545e80bd16
Simon Gussenhoven <simongussenhoven@gmail.com>
2024-02-09 22:58:31 +0100

    Fix backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,9 @@
 import * as mongoose from 'mongoose';
-
 import app from './app';
 
+mongoose.connect(process.env.DB_PATH).then(() => console.log('db connection succesfull'));
 
-
-const DB = process.env.DB || 'error';
-console.log(DB)
-mongoose.connect(DB).then(() => console.log('db connection succesfull'));
-
-const port = process.env.PORT || 3001;
+const port = process.env.BACKEND_PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
