0c737ee855041c12c4eb63f2b7f2ab254ba7373a
Simon Gussenhoven <simongussenhoven@gmail.com>
2024-02-24 21:19:37 +0000

    Fix cookie

--- apps/backend/src/controllers/auth/createSendToken.ts
+++ apps/backend/src/controllers/auth/createSendToken.ts
@@ -1,14 +1,15 @@
 import { IUser } from "@types";
 import { signToken } from '../../utils/signToken';
-import { Response } from 'express';
+import { CookieOptions, Response } from 'express';
 
 export const createSendToken = (user: IUser, statusCode: number, res: Response) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(Date.now() + Number(process.env.JTW_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000),
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true
-  }
+    httpOnly: true,
+    sameSite: "none"
+  } as CookieOptions
   res.cookie('jwt', token, cookieOptions)
   res.status(statusCode).json({
     status: 'success',
