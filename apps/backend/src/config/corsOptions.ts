4c9e065a41723b9dc10265e80805b4f7e4ba96e1
Simon Gussenhoven <simongussenhoven@gmail.com>
2024-03-04 23:05:56 +0100

    Last try

--- apps/backend/src/config/corsOptions.ts
+++ apps/backend/src/config/corsOptions.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from "vite";
 
 export default {
-  origin: ['/', 'http://localhost:3000', '/smikkelweb\.com$/'],
+  origin: ['/', 'http://localhost:3000', '/\.smikkelweb\.com$/'],
   credentials: true
 } as CorsOptions;
