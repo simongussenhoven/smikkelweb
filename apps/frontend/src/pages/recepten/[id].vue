f39a50c8b713d1474fb2ed5a7002a282ae929e7e
Simon Gussenhoven <simongussenhoven@gmail.com>
2024-03-10 14:36:36 +0100

    Fix single page

--- apps/frontend/src/pages/recepten/[id].vue
+++ apps/frontend/src/pages/recepten/[id].vue
@@ -3,36 +3,44 @@
     <div class="text-2xl font-bold text-center">Loading...</div>
   </div>
   <div v-else class="max-w-screen-xl flex flex-col justify-between mx-auto p-4">
-    <div class="container mx-auto p- bg-slate-900 rounded p-6">
-      <h1 class="title mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+    <div class="container mx-auto rounded p-6">
+      <h1 class="title mb-4 text-4xl font-extrabold tracking-tight leading-none text-dark dark:text-white md:text-5xl lg:text-6xl pb-5 ">
         {{ recipe.title }}
       </h1>
-      <div class="lg:flex">
-        <div class="lg:w-1/4">
-          <div class="bg-dark p-4 rounded shadow">
+      <div class="lg:flex gap-4">
+        <div class="lg:w-1/4 flex flex-col gap-4">
+          <div class="bg-dark p-4 rounded shadow bg-white dark:bg-gray-700 text-dark dark:text-white ">
             <h2 class="text-xl font-bold mb-2">Benodigdheden</h2>
             <ul class="list-disc pl-4">
               <li v-for="req in recipe.requirements">{{ req.name }}</li>
             </ul>
           </div>
+          <div class="bg-dark p-4 rounded shadow bg-white dark:bg-gray-700 text-dark dark:text-white">
+            <h2 class="text-xl font-bold mb-2">Ingredienten</h2>
+            <ul class="list-disc pl-4">
+              <li v-for="ing in recipe.ingredients">{{ `${ing.amount} ${ing.unit} ${ing.name.toLowerCase()}` }}</li>
+            </ul>
+          </div>
         </div>
-        <div class="lg:w-2/4 mt-4 lg:mt-0">
-          <div class="bg-dark p-4 rounded shadow">
+        <div class="lg:w-3/4 mt-4 lg:mt-0 flex flex-col gap-4">
+          <div class="bg-dark p-4 rounded shadow bg-white dark:bg-gray-700 text-dark dark:text-white">
             <h2 class="text-xl font-bold mb-2">Over dit recept</h2>
             <p>{{ recipe.description }}</p>
-            <div class="bg-dark mt-4 rounded shadow mb-4">
-              <h2 class="text-xl font-bold mb-2">Ingredients</h2>
-              <ul class="list-disc pl-4">
-                <li v-for="ing in recipe.ingredients">{{ `${ing.amount} ${ing.unit} ${ing.name.toLowerCase()}` }}</li>
-              </ul>
-            </div>
           </div>
-          <div class="bg-dark p-4 pt-0 rounded shadow">
-            <h2 class="text-xl font-bold mb-2">Instructies</h2>
+          <!-- <div class="bg-dark p-4 rounded shadow bg-white">
+            <h2 class="text-xl font-bold mb-2">Ingredienten</h2>
+            <ul class="list-disc pl-4">
+              <li v-for="ing in recipe.ingredients">{{ `${ing.amount} ${ing.unit} ${ing.name.toLowerCase()}` }}</li>
+            </ul>
+          </div> -->
+
+          <div class="bg-dark p-4 rounded shadow bg-white dark:bg-gray-700 text-dark dark:text-white">
+            <h2 class="text-xl font-bold mb-2">Ingredienten</h2>
             <ul class="list-disc pl-4">
               <li v-for="step in recipe.steps"> {{ step.description }}</li>
             </ul>
           </div>
+
         </div>
       </div>
     </div>
--- apps/frontend/src/pages/recepten/index.vue
+++ apps/frontend/src/pages/recepten/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="max-w-screen-xl flex flex-col justify-between mx-auto p-4 recipe-container">
-    <h1 class="title mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-dark dark:text-white">
-      Recepten
-    </h1>
+    <h1 class="title mb-4 text-4xl font-extrabold tracking-tight leading-none text-dark dark:text-white md:text-5xl lg:text-6xl pb-5 ">
+        Recepten
+      </h1>
     <div class="recipes flex flex-wrap gap-3">
       <SCard v-for="recipe in recipeStore.recipes" :key="recipe" :title="recipe.title"
         :description="recipe.description" :id="recipe._id" />
